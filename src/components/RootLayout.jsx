bda2fca5b82645b2849d54a935c05b2ab117bea8
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-04-22 15:17:54 +0530

    updated root style

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom"
 
 const RootLayout = () => {
   return (
-    <main className="min-h-[100vh] min-w-[100vw]">
+    <main className="min-h-[100vh] min-w-[100vw] bg-[#F7F8F9] font-['Rubik']">
       <Outlet />
     </main>
   )
